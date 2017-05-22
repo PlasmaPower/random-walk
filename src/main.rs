e562ff37ff92b110ad28fead247736411c07f171
Lee Bousfield <ljbousfield@gmail.com>
2017-05-22 14:09:42 -0600

    Add output formats

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::thread;
 use std::collections::HashMap;
 
 #[macro_use]
@@ -38,29 +39,60 @@ lazy_static! {
     };
 }
 
+fn random_walk(mut rng: &mut Rng, starting_room: &Room) -> u64 {
+    let mut hours = 0;
+    let mut room = starting_room;
+    loop {
+        hours += 1;
+        let next_room = room.random_door(&mut rng);
+        if next_room == "EXIT" {
+            break;
+        }
+        room = ROOMS.get(next_room).unwrap();
+    }
+    hours
+}
+
 fn main() {
     let args = load_yaml!("../cli.yml");
     let args = clap::App::from_yaml(args).get_matches();
-    let mut rng = rand::weak_rng();
     let n: usize = args.value_of("number").unwrap().parse().expect("Expected number of walks to be a positive integer");
+    let thread_count: usize = args.value_of("threads").unwrap().parse().expect("Expected number of threads to be a positive integer");
     let starting_room = ROOMS.get(args.value_of("starting_room").unwrap()).expect("Expected starting room to be a room");
+    let outputs = args.values_of("outputs").unwrap().collect::<Vec<_>>();
+    let raw_output = outputs.contains(&"raw");
+    let mut threads = Vec::new();
+    for i in 0..thread_count {
+        threads.push(thread::spawn(move || {
+            let mut rng = rand::weak_rng();
+            let mut our_sum: u64 = 0;
+            let mut our_squared_sum: u64 = 0;
+            let mut our_count = n / thread_count;
+            if i == thread_count - 1 {
+                our_count += n % thread_count;
+            }
+            for _ in 0..our_count {
+                let hours = random_walk(&mut rng, starting_room);
+                if raw_output {
+                    println!("{}", hours);
+                }
+                our_sum += hours;
+                our_squared_sum += hours * hours;
+            }
+            (our_sum, our_squared_sum)
+        }));
+    }
     let mut hours_sum: u64 = 0;
     let mut hours_squared_sum: u64 = 0;
-    for _ in 0..n {
-        let mut hours = 0;
-        let mut room = starting_room;
-        loop {
-            hours += 1;
-            let next_room = room.random_door(&mut rng);
-            if next_room == "EXIT" {
-                break;
-            }
-            room = ROOMS.get(next_room).unwrap();
-        }
-        hours_sum += hours;
-        hours_squared_sum += hours * hours;
+    for thread in threads {
+        let result = thread.join().unwrap();
+        hours_sum += result.0;
+        hours_squared_sum += result.1;
     }
-    let mean = (hours_sum as f64) / (n as f64);
-    println!("mean: {}", mean);
-    println!("stdev: {}", ((hours_squared_sum as f64) / ((n - 1) as f64) - mean.powi(2)).sqrt());
+    if outputs.contains(&"mean") {
+        println!("mean: {}", (hours_sum as f64) / (n as f64));
+    }
+    if outputs.contains(&"stdev") {
+        println!("stdev: {}", (((hours_squared_sum as f64) - ((hours_sum * hours_sum) as f64) / (n as f64)) / ((n - 1) as f64)).sqrt());
+    }
 }
